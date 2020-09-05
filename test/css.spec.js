721470438759a84759694939f781b2f5aa6f569d
hewIngram <hew@gousto.co.uk>
2020-09-05 13:51:17 +0100

    feat/handle external css and external css modules

--- test/css.spec.js
+++ test/css.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import Css from './resources/Css.vue'
+import ExternalCss from './resources/ExternalCss.vue'
 import StyleModule from './resources/StyleModule.vue'
 
 describe('processes .vue file with Css style', () => {
@@ -22,5 +23,27 @@ describe('processes .vue file with Css style', () => {
 
   it('handles style modules', () => {
     shallowMount(StyleModule)
+  })
+})
+
+describe('processes .vue file with external Css style', () => {
+  let wrapper
+  beforeAll(() => {
+    wrapper = shallowMount(ExternalCss)
+  })
+
+  it('should bind from style tags with an anonymous module', () => {
+    expect(wrapper.classes()).toContain('testA')
+    expect(wrapper.find('.testC').text()).toBe('Test text')
+  })
+
+  it('should bind from style tags with a named module', () => {
+    expect(wrapper.classes()).toContain('testB')
+  })
+
+  it('should bind from style tags', () => {
+    const innerElement = wrapper.find('.testD')
+    expect(innerElement.text()).toBe('lorem ipsum')
+    expect(innerElement.classes()).toContain('testD')
   })
 })
---
+++ test/resources/ExternalCss.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="[css.testB, $style.testA]">
+    <div :class="$style.testC">Test text</div>
+    <div class="testD">lorem ipsum</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+}
+</script>
+
+<style module src="./ExternalCss.css" />
+<style module="css" src="./ExternalNamedCss.css" />
+<style>
+.testD {
+    color: green;
+}
+</style>
